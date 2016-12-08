c47e9af41d1762e4e002190c0a52904bc856aa15
Edward Kandrot <Edward Kandrot>
2016-12-07 22:49:38 -0800

    created new main, renders single square, basis for grid game

---
+++ Win32Project1/Game0.cpp
@@ -0,0 +1,348 @@
+#include <stdio.h>
+#include <ctime>
+#include <cstdlib>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <GL/glew.h>
+#include "shaders.h"
+#include "Texture.h"
+#include "maths.h"
+
+#define GLFW_INCLUDE_GLU
+#define GLFW_INCLUDE_GLEXT
+#include <GLFW/glfw3.h>
+
+//#############################################################################
+
+const GLfloat GRAVITY = -0.001f;
+const GLfloat MAX_STEP_HEIGHT = 0.55f;
+
+enum MovementState {
+    OnGround,
+    Falling,
+    Flying,
+    Swimming,
+    God,
+};
+
+//#############################################################################
+
+struct Player {
+
+    Player();
+
+    void get_eye(float *look) const;
+    void get_pos(float *look) const;
+
+    void update_gaze(GLfloat elevationDelta, GLfloat rotationDelta);
+
+    const float PLAYER_HEIGHT = 1.5f;
+    
+    // position and view within the world
+    float _eyeHeight = 1.5f;
+    float _pos[3] = { 0,0,0 };
+    float _facing = 45 * 3.14159f / 180;    // radians left/right around player axis
+    float _facingUpDown = -45 * 3.14159f / 180;    // radians up/down with player eyes as level zero
+
+                                                   // velocities
+    float _upV = 0; // negative is down
+    float _rightV = 0;  // negative is left
+    float _forwardV = 0; // negative is backwards
+    MovementState _movementState = OnGround;
+};
+
+
+static Player player;
+
+Player::Player() {
+}
+
+void Player::get_eye(float *look) const {
+    look[0] = _pos[0] + cosf(_facingUpDown) * cosf(_facing);
+    look[1] = _pos[1] + cosf(_facingUpDown) * sinf(_facing);
+    look[2] = _pos[2] + sinf(_facingUpDown) + PLAYER_HEIGHT;
+}
+
+void Player::get_pos(float *pos) const {
+    pos[0] = _pos[0];
+    pos[1] = _pos[1];
+    pos[2] = _pos[2] + PLAYER_HEIGHT;
+}
+
+void Player::update_gaze(GLfloat elevationDelta, GLfloat rotationDelta) {
+    _facingUpDown -= elevationDelta;
+    _facingUpDown = max(-3.14159f / 2, _facingUpDown);
+    _facingUpDown = min(3.14159f / 2, _facingUpDown);
+
+    _facing -= rotationDelta;
+}
+
+//#############################################################################
+/*======================================================================*
+* glfw interface code
+*======================================================================*/
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    // first check for escape
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+        return;
+    }
+
+    // check for moving keys, if in a moving-mode
+    int forwardMotion = 0;
+    int sidewaysMotion = 0;
+    int upMotion = 0;
+
+    bool forwardKey = glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS;
+    bool backKey = glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS;
+    bool leftKey = glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS;
+    bool rightKey = glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS;
+    bool jumpKey = glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS;
+    bool diveKey = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS;
+
+    if (forwardKey && !backKey) {
+        forwardMotion = 1;
+    } else if (backKey && !forwardKey) {
+        forwardMotion = -1;
+    }
+    if (leftKey && !rightKey) {
+        sidewaysMotion = 1;
+    } else if (rightKey && !leftKey) {
+        sidewaysMotion = -1;
+    }
+
+    if (jumpKey && !diveKey) {
+        upMotion = 1;
+    } else if (diveKey && !jumpKey) {
+        upMotion = -1;
+    }
+    //if (key == GLFW_KEY_F && action == GLFW_PRESS) {
+    //    if (playerState == Flying) {
+    //        playerState = Falling;
+    //    } else {
+    //        playerState = Flying;
+    //    }
+    //}
+
+    //update_player_motion(forwardMotion, sidewaysMotion, upMotion);
+}
+
+static double xposPrev, yposPrev;   // gets initialized in init_glfw()
+const GLfloat SCALED_MOUSE_MOVEMENT = 0.001f;
+static void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos) {
+    GLfloat elevationDelta = (GLfloat)(ypos - yposPrev) * SCALED_MOUSE_MOVEMENT;
+    GLfloat rotationDelta = (GLfloat)(xpos - xposPrev) * SCALED_MOUSE_MOVEMENT;
+    yposPrev = ypos;
+    xposPrev = xpos;
+    player.update_gaze(elevationDelta, rotationDelta);
+}
+
+static void error_callback(int error, const char* description) {
+    fputs(description, stderr);
+}
+
+//#############################################################################
+
+static GLFWwindow* init_glfw() {
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit()) {
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); // We want OpenGL 4.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_DEPTH_BITS, 16);
+
+    GLFWwindow* window = glfwCreateWindow(960, 960, "Masterful", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    //glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, cursor_pos_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwGetCursorPos(window, &xposPrev, &yposPrev);
+    return window;
+}
+
+static void shutdown_glfw(GLFWwindow *window) {
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
+
+//#############################################################################
+GLuint CubeID;
+GLuint SquareTexturedVA;
+TextureShader *textureShader;
+Texture *brick1Tex;
+
+
+struct Scene {
+    float _ratio;
+
+    Scene(float ratio) : _ratio(ratio) {}
+
+    void render(const float *pos, const float *look);
+};
+
+void Scene::render(const float *pos, const float *look) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    float MVP[16], view[16], projection[16];
+    lookAt(pos[0], pos[1], pos[2], look[0], look[1], look[2], 0, 0, 1, view);
+    perspective(45, _ratio, 0.1f, 100.0f, projection);
+    matmul(projection, view, MVP);
+
+    // draw a test square
+    {
+        glBindVertexArray(SquareTexturedVA);
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        textureShader->use();
+        brick1Tex->bind();
+        glActiveTexture(GL_TEXTURE0);
+        glUniform1i(textureShader->samplerID, 0);
+        GLfloat model[16] = { 1,0,0,0, 0,1,0,0, 0,0,1,0, -0.5,-0.5,0,1 };
+        GLfloat lineMVP[16];
+        matmul(MVP, model, lineMVP);
+        glUniformMatrix4fv(textureShader->matrixID, 1, GL_FALSE, lineMVP);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // 3 vertex per triangle
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+    }
+}
+
+
+void init_opengl_objects() {
+    GLuint tempBufID;
+
+//    GLuint SquareTexturedVA;
+    glGenVertexArrays(1, &SquareTexturedVA);
+    glBindVertexArray(SquareTexturedVA);
+    GLfloat squareData[4 * 2*2] = {
+        // 4 sets of:  2d coords, 2d tex coords
+        0,0, 0,0,  0,1, 0,1,  1,0, 1,0,  1,1, 1,1 };
+    GLint squareIndexes[8 * 4] = { 0,1,2, 1,2,3 };
+
+    glGenBuffers(1, &tempBufID);
+    glBindBuffer(GL_ARRAY_BUFFER, tempBufID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(squareData), squareData, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &tempBufID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, tempBufID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareIndexes), squareIndexes, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,  // attribute 0.
+        2,  // size
+        GL_FLOAT,   // type
+        GL_FALSE,   // normalized?
+        4 * sizeof(GLfloat),  // stride
+        (void*)0 // array buffer offset
+    );
+
+    glVertexAttribPointer(
+        1,
+        2,  // size
+        GL_FLOAT,   // type
+        GL_FALSE,   // normalized?
+        4 * sizeof(GLfloat),  // stride
+        (void*)(2 * sizeof(GLfloat)) // array buffer offset
+    );
+
+
+//    GLuint CubeID;
+    glGenVertexArrays(1, &CubeID);
+    glBindVertexArray(CubeID);
+    GLfloat cubeData[12 * 5] = {
+        0,0,0, 0,0,  0,1,0, 1,0,  0,1,1, 1,1,  0,0,1, 0,1,  1,1,0, 0,0,  1,1,1, 0,1,  1,0,0, 1,0,  1,0,1, 1,1,
+        1,0,1, 0,0,  0,0,1, 1,0,  1,0,0, 0,1,  0,0,0, 1,1 };
+    GLint cubeIndexes[8 * 4] = { 0,1,2,3, 1,4,5,2, 4,6,7,5, 6,0,3,7, 2,5,8,9, 1,4,10,11 };
+
+    glGenBuffers(1, &tempBufID);
+    glBindBuffer(GL_ARRAY_BUFFER, tempBufID);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeData), cubeData, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &tempBufID);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, tempBufID);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndexes), cubeIndexes, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,  // size
+        GL_FLOAT,   // type
+        GL_FALSE,   // normalized?
+        5 * sizeof(GLfloat),  // stride
+        (void*)0 // array buffer offset
+    );
+
+    glVertexAttribPointer(
+        1,
+        2,  // size
+        GL_FLOAT,   // type
+        GL_FALSE,   // normalized?
+        5 * sizeof(GLfloat),  // stride
+        (void*)(3 * sizeof(GLfloat)) // array buffer offset
+    );
+
+
+    // clear the bound buffer array
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    
+    
+    textureShader = new TextureShader("..\\shaders\\SimpleTextureShader.vert", "..\\shaders\\SimpleTextureShader.frag");
+    brick1Tex = new Texture(std::wstring(L"..\\textures\\brick_0.jpg"));
+
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+}
+
+//#############################################################################
+
+
+//#############################################################################
+
+int main(void) {
+    GLFWwindow *window = init_glfw();
+    init_opengl_objects();
+    //init_game_objects();
+
+    float ratio;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float)height;
+    glViewport(0, 0, width, height);
+    Scene scene(ratio);
+
+    clock_t startTime = clock(); //Start timer
+    while (!glfwWindowShouldClose(window)) {
+
+        float pos[3];
+        float look[3];
+        player.get_pos(pos);
+        player.get_eye(look);  // get eye so we know which direction forward is
+        scene.render(pos, look);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        //update_object_locations();
+    }
+    shutdown_glfw(window);
+}
---
+++ Win32Project1/maths.h
@@ -0,0 +1,98 @@
+#pragma once
+
+//#############################################################################
+
+#define max(a, b) ( (a) < (b) ? (b) : (a) )
+#define min(a, b) ( (a) > (b) ? (b) : (a) )
+#define sqr(x) (x*x)
+
+//#############################################################################
+
+void normalize(float *vec) {
+    float d = sqrt(sqr(vec[0]) + sqr(vec[1]) + sqr(vec[2]));
+    vec[0] /= d;
+    vec[1] /= d;
+    vec[2] /= d;
+}
+
+float dot(const float *a, const float *b) {
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+// c = a cross b
+void cross(const float *a, const float *b, float *c) {
+    c[0] = (a[1] * b[2]) - (a[2] * b[1]);
+    c[1] = (a[2] * b[0]) - (a[0] * b[2]);
+    c[2] = (a[0] * b[1]) - (a[1] * b[0]);
+}
+
+void matmul(const float *a, const float *b, float *c) {
+    for (int row = 0; row<4; ++row) {
+        for (int col = 0; col<4; ++col) {
+            float sum = 0;
+            for (int i = 0; i<4; ++i) {
+                sum += a[row + 4 * i] * b[i + 4 * col];
+            }
+            c[row + 4 * col] = sum;
+        }
+    }
+}
+
+void lookAt(float eyex, float eyey, float eyez, float centerx, float centery, float centerz, float upx, float upy, float upz, float *m) {
+    float Z[3] = { eyex - centerx, eyey - centery, eyez - centerz };
+    normalize(Z);
+    float X[3];
+    float Y[3] = { upx, upy, upz };
+    cross(Y, Z, X);
+    cross(Z, X, Y);
+    normalize(X);
+    normalize(Y);
+
+    float eye[3] = { eyex, eyey, eyez };
+    m[0] = X[0];
+    m[4] = X[1];
+    m[8] = X[2];
+    m[12] = -dot(X, eye);
+
+    m[1] = Y[0];
+    m[5] = Y[1];
+    m[9] = Y[2];
+    m[13] = -dot(Y, eye);
+
+    m[2] = Z[0];
+    m[6] = Z[1];
+    m[10] = Z[2];
+    m[14] = -dot(Z, eye);
+
+    m[3] = 0;
+    m[7] = 0;
+    m[11] = 0;
+    m[15] = 1;
+}
+
+// redefine near/far  as n/f, due to Windows headers
+void perspective(float angle, float ratio, float n, float f, float *p) {
+    float e = 1 / tanf(angle * 3.14159f / 180 / 2);
+    p[0] = e;
+    p[1] = 0;
+    p[2] = 0;
+    p[3] = 0;
+
+    p[4] = 0;
+    p[5] = e * ratio;
+    p[6] = 0;
+    p[7] = 0;
+
+    p[8] = 0;
+    p[9] = 0;
+    p[10] = -(f + n) / (f - n);
+    p[11] = -1;
+
+    p[12] = 0;
+    p[13] = 0;
+    p[14] = -2 * f * n / (f - n);
+    p[15] = 0;
+}
+
+//#############################################################################
+
